c7181d0296b5c1055755160d0ad3461107190a19
Your Name <you@example.com>
2024-03-21 04:55:10 +0200

    [Frontend] Add reusable validate input field component

---
+++ frontend/src/components/ValidateInputField/ValidateInputField.tsx
@@ -0,0 +1,21 @@
+import { TextField, BaseTextFieldProps } from "@mui/material";
+import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+
+interface ValidatedInputFieldProps extends Omit<BaseTextFieldProps, "error"> {
+  error?: FieldError;
+  register?: UseFormRegisterReturn;
+  label: string;
+}
+
+export const ValidatedInputField = ({ error, register, label, ...props }: ValidatedInputFieldProps) => {
+  return (
+    <TextField
+      error={error ? true : false}
+      id="validated-input"
+      label={label}
+      {...register}
+      {...props}
+      helperText={error ? error.message : " "}
+    />
+  );
+};
