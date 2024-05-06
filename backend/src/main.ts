ff3316e7a5efb1e9fcc256ecf234cf8a788906db
Katarhu <mkaimg1@gmail.com>
2024-05-06 20:10:37 +0300

    [Backend] Enable corse

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,7 @@ import { AppModule } from "./AppModule";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(AppConfig.globalPrefix);
   app.use(cookieParser());
